291af3da5ab9c41681a2076f472d061d27fc4dc6
Andy <andy9775@gmail.com>
2017-01-12 14:06:54 -0500

    write integration test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test 'layout links' do
+    get root_path
+    assert_template 'static_pages/home' # specify the directory (assumed its app/views)
+    # test for the presence of a particular link (<a> tag)
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    get contact_path
+    assert_select 'title', full_title('Contact')
+  end
+end
