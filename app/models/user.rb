147f1b9ff0a9a1512d1aadddc2ff6bbc3f9477e8
Andy <andy9775@gmail.com>
2017-01-12 18:51:06 -0500

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # self.email = self.email.downcase
+  # self.email = email.downcase
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 225 },
+                    format: { with:
+                      /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
 end
