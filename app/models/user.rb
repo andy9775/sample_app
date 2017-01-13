00e9c0175ffac599a4a077c4c5031488fb028c53
Andy <andy9775@gmail.com>
2017-01-12 20:43:03 -0500

    Make user model with secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
                     format: { with:
                       /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
+  has_secure_password # class methods
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,18 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new name: 'Example User', email: 'user@example.com'
+    @user = User.new name: 'Example User', email: 'user@example.com',
+                     password: 'foobar', password_confirmation: 'foobar'
+  end
+
+  test 'password should be present' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
+
+  test 'password should have minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
   end
 
   test 'should be valid' do
