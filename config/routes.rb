b90cb6100956247ca6f020f83d32020861ca8b7a
Andy <andy9775@gmail.com>
2017-01-16 16:46:50 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # calling context is: #<ActionDispatch::Routing::Mapper:0x007f8114f55b80>
+  # a class instance
+  # has methods of ActionDispatch::Routing::Mapper -> get, resources, ...
+
   root 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
@@ -11,4 +15,6 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+	post '/signup', to: 'users#create'
+  resources :users
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+	test 'invalid signup information' do
+		get signup_path
+		assert_select 'form[action="/signup"]'
+		assert_no_difference 'User.count' do
+			post signup_path, params: {
+				user: {
+					name: '',
+					email: 'user@invalid.com',
+					password: 'foo',
+					password_confirmation: 'bar' } }
+		end
+
+		# assert error explanation is visible
+		assert_select 'div[id="error_explanation"]'
+
+		# assert error indicators on labels
+		assert_select 'div[class="field_with_errors"]' do
+			assert_select 'label[for="user_name"]'
+			assert_select 'label[for="user_password"]'
+			assert_select 'label[for="user_password_confirmation"]'
+		end
+
+		# assert new template is rendered
+		assert_template 'users/new'
+	end
+
+	test 'valid signup information' do
+		get signup_path
+		assert_difference 'User.count', 1 do
+			post users_path, params: {
+					user: {
+						name: 'Example User',
+						email: 'user@example.com',
+						password: 'password',
+						password_confirmation: 'password' } }
+		end
+
+		follow_redirect! # go to the next page
+		assert_template 'users/show'
+		assert_not flash.empty?
+		assert_select 'div[class="alert alert-success"]'
+	end
+end
