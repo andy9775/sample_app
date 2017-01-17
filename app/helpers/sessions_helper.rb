4b15a75a6d92e6fa5f9abf4b609b67a5fa373ccd
Andy <andy9775@gmail.com>
2017-01-17 14:07:48 -0500

    add persistant sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,19 +5,47 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # remembers to user for a permanent session
+  def remember(user)
+    # create a new remember token and save to DB
+    user.remember
+    # assign user id to cookie and sign it
+    cookies.permanent.signed[:user_id] = user.id
+    # add remember token to the cookie. **Permanent** sets it for 20 years
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # logout the current user
   def log_out
     # session object/class contains methods to mutate the session state.
     # Each session object is available for each request. This means that we can
     # validate or invalidate a session. The session object is built based on the
     # session cookie data sent by the client
+    forget current_user # method. same as self.current_user
     session.delete :user_id
     @current_user = nil
   end
 
   # Returns the current logged-in user (if any)
   def current_user
-    @current_user ||= User.find_by id: session[:user_id]
+    # if short session exists, use that
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id]) # long term session cookie
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token]) # is valid?
+        log_in user # log user in and set a session
+        @current_user = user
+      end
+    end
+    # else nothing exists for the user id or the long term cookie is invalid
   end
 
   # determines if there is a logged in user. Returns true if user is logged in
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # self.email = self.email.downcase
   # self.email = email.downcase
   before_save { email.downcase! }
+  attr_accessor :remember_token
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 225 },
                     format: { with:
@@ -10,10 +11,41 @@ class User < ApplicationRecord
   has_secure_password # class methods
   validates :password, presence: true, length: { minimum: 6 }
 
-  # Returns that hash digest of the given string
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  # Remembers a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  # forget a user
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  # Returns true if the given token matches the digest
+  # Uses the users remember_digest to compare against the provided
+  # remember_token to determine if they are the same thing
+  def authenticated?(remember_token)
+    # if the remember digest is not set (e.g. we logout of one browser which
+    # sets it to nil see forget method above) then we know that the user is not
+    # authenticated
+    return false if remember_digest.nil?
+    # remember_digest refers to self.remember_digest - the DB column
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # class methods
+  class << self
+    # Returns that hash digest of the given string
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # returns a randomly generated token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 end
